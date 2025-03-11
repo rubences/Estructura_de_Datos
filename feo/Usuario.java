f8b7fdee9a854f683c8cac15259939bb0dfd291b
rubences <91731294+rubences@users.noreply.github.com>
2025-03-11 12:06:31 +0000

    usuario

--- feo/Usuario.java
+++ feo/Usuario.java
@@ -8,4 +8,19 @@ public class Usuario {
     }
 
     // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
